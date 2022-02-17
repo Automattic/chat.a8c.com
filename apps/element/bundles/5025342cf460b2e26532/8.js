a259482613fef54531f822b00d0f9e7ed436ea6f
Team Orbit <orbit-matrix@automattic.com>
2022-02-17 16:15:41 +0000

    Update Element to v1.10.4

--- apps/element/bundles/5025342cf460b2e26532/8.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{1032:function(n,w,o){"use strict";o.r(w)}}]);
-//# sourceMappingURL=8.js.map
---
+++ apps/element/bundles/f2df67371ec7a840fddf/8.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{1032:function(n,w,o){"use strict";o.r(w)}}]);
+//# sourceMappingURL=8.js.map
